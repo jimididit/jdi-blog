579f63a8cc80578446dec2a952c3a783a5a5c549
jimididit <jimi@jimididit.com>
2024-12-06 22:33:19 -0500

    Update github url

--- src/utils/media.ts
+++ src/utils/media.ts
@@ -12,4 +12,4 @@ export type MediaExternallyHostedVideo = {
 };
 
 export const DEFAULT_MEDIA_URL: GithubContentURL =
-	"https://api.github.com/repos/one-aalam/astro-ink/contents/src/data/astro-media.json";
+	"https://api.github.com/repos/jimididit/jdi-blog/contents/src/data/astro-media.json";
