7ee44cefa55602f9166e5f4be58804293e6d49b6
jimididit <jimi@jimididit.com>
2024-12-08 02:52:17 -0500

    Update theme

--- tailwind.theme.config.cjs
+++ tailwind.theme.config.cjs
@@ -142,15 +142,30 @@ module.exports = {
         }
     },
     /**
-     * Color Palette - Default/Duplicate of Purple Heart (Never remove this)
+     * Color Palette - Default/Duplicate of Pink Town (Never remove this)
      */
     default: {
+        // colors: {
+        //     primary: colors.purple[700],
+        //     secondary: colors.purple[800],
+        //     dark: {
+        //         primary: colors.purple[300],
+        //         secondary: colors.purple[500]
+        //     },
+        //     accent: {
+        //         gray: {
+        //             light: colors.gray[300],
+        //             dark: colors.gray[500]
+        //         },
+        //         default: colors.blue[700]
+        //     }
+        // }
         colors: {
-            primary: colors.purple[700],
-            secondary: colors.purple[800],
+            primary: colors.pink[700],
+            secondary: colors.pink[800],
             dark: {
-                primary: colors.purple[300],
-                secondary: colors.purple[500]
+                primary: colors.pink[300],
+                secondary: colors.pink[500]
             },
             accent: {
                 gray: {
